fc2721f3f18557451a21b9cc874cc4c61cb4d334
SashaLeshiy <zomlesh@yandex.ru>
2021-10-25 22:39:20 +0300

    изменение урл в аутентификации

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://hitapi.roooar.ru';
+export const BASE_URL = 'https://hitapi.roooar.ru';
 
 const checkResponse = (response) => {
   return response.ok ? response.json() :
